324108589d348d4f041a5d6956028c6bd916df0a
keyeypee <keyeypee@gmail.com>
2020-06-07 22:44:31 +0530

    Updates

---
+++ precache-manifest.a0e0aa3a8dd324d3151de4d0b3192e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc816f623d4fd7edd918b235e356d54",
+    "url": "/dates-recipies-simulation/index.html"
+  },
+  {
+    "revision": "a5bd110f64deaef59854",
+    "url": "/dates-recipies-simulation/static/css/main.e69f611a.chunk.css"
+  },
+  {
+    "revision": "7513ad65bdd9b0e4430b",
+    "url": "/dates-recipies-simulation/static/js/2.205df9a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dates-recipies-simulation/static/js/2.205df9a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5bd110f64deaef59854",
+    "url": "/dates-recipies-simulation/static/js/main.e5eb89f7.chunk.js"
+  },
+  {
+    "revision": "09497f7e25319a6010d4",
+    "url": "/dates-recipies-simulation/static/js/runtime-main.0570abb5.js"
+  }
+]);
